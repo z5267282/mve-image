11bdc878e867564ab8131000cd76c840bffdc416
Sunny Chen <z5267282@student.unsw.edu.au>
2024-05-20 15:04:16 +1000

    attempting to link folder

--- start-container.sh
+++ start-container.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env dash
+
+# note that dash is not always installed in /bin/dash
+# but env is always in /usr/bin/env
+
 # softlink mounted files to public folder of server
 ln -s /mnt/bob videos
 
