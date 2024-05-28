d37f4a0d8d1b57da3f0dba625ff1aaba70ca451a
Sunny Chen <z5267282@student.unsw.edu.au>
2024-05-28 23:53:49 +1000

    static playing works

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 
 function App() {
-  const item = 'disgrace.mp4';
-
-  if (item !== null) {
-    document.title = item;
-  }
-
-  return (item === null) ?
-    <div>waiting for a video to be played</div>
-  :
     // cannot autoplay unless video is muted as a <video tag limitation
-    <div style={{width: "50vw"}}>
-      <video controls autoPlay muted src={`/videos/${item}`}/>
+    return <div style={{width: "50vw"}}>
+      <video controls autoPlay muted src={`/videos/disgrace.mov`}/>
     </div>
 }
 
