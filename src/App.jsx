bbabd34cbaadd84cf0eb2be7b7c8918a64be47d2
Sunny Chen <z5267282@student.unsw.edu.au>
2024-05-29 00:47:00 +1000

    server side event nearly works

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 function App() {
+  const [video, setVideo] = React.useState('disgrace.mov');
+
+    React.useEffect(() => {
+      const eventSource = new EventSource('/play');
+
+      eventSource.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        setVideo(data.vid);
+      };
+
+      return () => eventSource.close();
+    }, []);
     // cannot autoplay unless video is muted as a <video tag limitation
     return <div style={{width: "50vw"}}>
-      <video controls autoPlay muted src={`/videos/disgrace.mov`}/>
+      <video controls autoPlay muted src={`/videos/${video}`}/>
     </div>
 }
 
