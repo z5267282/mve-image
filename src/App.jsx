3ac4dfe9939cfb114ccf6fa82e9ccbc0a0fd9a63
Sunny Chen <z5267282@student.unsw.edu.au>
2024-06-04 00:43:20 +1000

    removing web socket and trying service worker

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from 'react';
-import io from 'socket.io-client';
-
-const socket = io('ws://localhost:6000');
 
 function App() {
-  const [video, setVideo] = React.useState('disgrace.mov');
-
-    useEffect(() => {
-      socket.on('connect', () => {
-          console.log('Connected to WebSocket');
-      });
-
-      socket.on('message', (vid) => {
-          setVideo(vid);
-      });
-
-      return () => {
-          socket.disconnect();
-      };
-    }, []);
+  const [video] = React.useState('disgrace.mov');
 
     // cannot autoplay unless video is muted as a <video tag limitation
     return <div style={{width: "50vw"}}>
