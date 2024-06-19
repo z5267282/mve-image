8113ea19471ba0e491416bda6924830955a1eb4c
Sunny Chen <z5267282@student.unsw.edu.au>
2024-06-19 18:17:33 +1000

    basic idea works, refactoring

--- mve.py
+++ mve.py
@@ -1,47 +1,54 @@
+import argparse
 import json
 import os
 import pathlib
 import subprocess
 import sys
 
-CONFIGS_KEY: str = "MVE_CONFIGS"
 
-CONFIG_BASENAME: str = "config.json"
+def main():
+    CONFIGS_KEY: str = 'MVE_CONFIGS'
 
-configs_path: str = os.environ.get(CONFIGS_KEY, None)
-if configs_path is None:
-    print(
-        f"the environment variable ${CONFIGS_KEY} has not been set", file=sys.stderr)
-    sys.exit(1)
+    CONFIG_BASENAME: str = 'config.json'
 
-config, = sys.argv[1:]
+    configs_path: str = os.environ.get(CONFIGS_KEY, None)
+    if configs_path is None:
+        print(
+            f'the environment variable ${CONFIGS_KEY} has not been set', file=sys.stderr)
+        sys.exit(1)
 
-config_file = os.path.join(configs_path, config, CONFIG_BASENAME)
-try:
-    with open(config_file, "r") as c:
-        cfg: dict = json.load(c)
-except FileNotFoundError:
-    print(
-        f"the config file ${config_file.replace(configs_path, CONFIGS_KEY)} does not exist", file=sys.stderr)
-    sys.exit(2)
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument('config', type=str)
+    args = parser.parse_args()
+
+    config: str = args.config
+
+    config_file: str = os.path.join(configs_path, config, CONFIG_BASENAME)
+    try:
+        with open(config_file, 'r') as c:
+            cfg: dict = json.load(c)
+    except FileNotFoundError:
+        print(
+            f'the config file ${config_file.replace(configs_path, CONFIGS_KEY)} does not exist', file=sys.stderr)
+        sys.exit(2)
+
+    set_host_docker_path_pair(cfg, 'SOURCE')
+    set_host_docker_path_pair(cfg, 'RENAMES')
+    set_host_docker_path_pair(cfg, 'DESTINATION')
+
+    configs_docker: str = pathlib.PurePosixPath(
+        *os.path.split(configs_path)).as_posix()
+
+    os.environ[CONFIGS_KEY] = configs_docker
+
+    # subprocess.run(['docker-compose', 'up', '--build'])
 
 
-# def create_abs_docker_path_pair(paths: list[str]) -> tuple[str, str]:
-#     return os.path.join(*paths), pathlib.PurePosixPath(*paths)
-
-def set_host_docker_path_pair(key: str):
+def set_host_docker_path_pair(cfg: dict, key: str):
     paths_list: list[str] = cfg[key]
-    os.environ[f"{key}_HOST"] = os.path.join(*paths_list)
-    os.environ[f"{key}_DOCKER"] = pathlib.PurePosixPath(*paths_list).as_posix()
+    os.environ[f'{key}_HOST'] = os.path.join(*paths_list)
+    os.environ[f'{key}_DOCKER'] = pathlib.PurePosixPath(*paths_list).as_posix()
 
 
-set_host_docker_path_pair("SOURCE")
-set_host_docker_path_pair("RENAMES")
-set_host_docker_path_pair("DESTINATION")
-
-configs_docker: str = pathlib.PurePosixPath(
-    *os.path.split(configs_path)).as_posix()
-
-os.environ[CONFIGS_KEY] = configs_docker
-
-subprocess.run(["docker-compose", "up", "--build"])
+if __name__ == '__main__':
+    main()
