6b5e2e819cc8e4d3044bef8e55a90243e95fd0e3
Sunny Chen <z5267282@student.unsw.edu.au>
2024-06-19 19:28:52 +1000

    adding clone of mve

--- start-container.sh
+++ start-container.sh
@@ -5,7 +5,8 @@
 
 # to make it easy, we serve static files from the current folder
 # we just symlink the original source folder to the current directory
+ln -s "$SOURCE_DOCKER" videos
 
-[ -L videos ] || ln -s "$SOURCE_DOCKER" videos
+git clone --depth=1 https://github.com/z5267282/mve.git && rm -rf mve/.git
 
 npm run start
